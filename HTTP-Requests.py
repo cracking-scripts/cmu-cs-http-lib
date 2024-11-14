f71e054cbcf91842ddb29b5f7fc72dd5c82fcafd
Null <thexbotsyt@gmail.com>
2024-11-14 10:36:29 -0500

    Update HTTP-Requests.py

--- HTTP-Requests.py
+++ HTTP-Requests.py
@@ -2,7 +2,7 @@ import browser.aio as aio
 
 async def request(url1: str, method1: str):
 	req = await aio.ajax(method=method1,url=url1)
-    return req.data
+	return req.data
 
 async def main():
 	# Put your code here. This is the only place you can get the data from requests.
